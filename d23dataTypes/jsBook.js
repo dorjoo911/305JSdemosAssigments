1cfb3b01ddcbe1e5b06eeb01067e4c7244247385
dorjoo911 <tomike@asia.com>
2022-03-02 20:09:51 -0600

    030222 assignments

--- d23dataTypes/jsBook.js
+++ d23dataTypes/jsBook.js
@@ -1,9 +1,10 @@
-// Primitives:  Can I add a string property?
+// *** Primitives:  Can I add a string property?
 let str = "Hello";
 str.test = 5; // (*)
 console.log(str.test);
 
-//Sum numbers from the visitor!
+// *** Sum numbers from the visitor!
+
 // let a = +prompt("The first number?", "");
 // let b = +prompt("The second number?", "");
 
@@ -23,15 +24,15 @@ console.log(str.test);
 // }
 // alert(`Read: ${readNumber()}`);
 
-// An occasional infinite loop
+// *** An occasional infinite loop
 let i = 0;
 while (i < 11) {
   i += 0.2;
   if (i > 9.8 && i < 10.2) console.log(i);
 }
 
-// A random number from min to max
-// (write a Mocha test with several different tests)
+// *** A random number from min to max
+// *** (write a Mocha test with several different tests)
 function random(min, max) {
   return min + Math.random() * (max - min);
 }
@@ -39,7 +40,7 @@ console.log(random(1, 5));
 console.log(random(1, 5));
 console.log(random(1, 5));
 
-// Is array copied?
+// *** Is array copied?
 let fruits = ["Apples", "Pear", "Orange"];
 
 let shoppingCart = fruits;
@@ -48,14 +49,14 @@ shoppingCart.push("Banana");
 
 console.log(fruits.length); // 4
 
-// Array operations
+// *** Array operations
 let styles = ["Jazz", "Blues"];
 styles.push("Rock-n-Roll");
 styles[Math.floor((styles.length - 1) / 2)] = "Classics";
 console.log(styles.shift());
 styles.unshift("Rap", "Reggae");
 
-// Calling in an array context
+// *** Calling in an array context
 let arr = ["a", "b"];
 
 arr.push(function () {
@@ -64,7 +65,8 @@ arr.push(function () {
 
 arr[2](); // a,b,function(){...}
 
-// Sum Imput
+// *** Sum Imput
+
 // function sumInput() {
 //   let numbers = [];
 
@@ -86,7 +88,7 @@ arr[2](); // a,b,function(){...}
 
 // console.log(sumInput());
 
-// A maximal subarray
+// *** A maximal subarray
 function getMaxSubSum(arr) {
   let maxSum = 0; // if we take no elements, zero will be returned
 
